19f91d4c4aa1f6b11d2713e819216455ae161eee
murkrow <ducdt1992@gmail.com>
2021-05-12 09:40:52 +0700

    add home screen check

--- App/Containers/HomeScreen/HomeScreen.jsx
+++ App/Containers/HomeScreen/HomeScreen.jsx
@@ -1,14 +1,48 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { Text, RefreshControl, ScrollView } from 'react-native';
+import { TOMATO, WHITE } from 'Themes/Colors';
 import { WebView } from 'react-native-webview';
 import HeaderBar from './HeaderBar/HeaderBar';
 import styles from './HomeScreenStyle';
 
 function HomeScreen() {
+  const webview = useRef(null);
+
+  const [scrollViewHeight, setScrollViewHeight] = useState(0);
+  const wevViewStyle = {
+    height: scrollViewHeight
+  };
+
+  function onLayout(event) {
+    setScrollViewHeight(event.nativeEvent.layout.height);
+  }
+
+  function onRefresh() {
+    webview.current.reload();
+  }
+
   return (
     <>
       <HeaderBar />
-      <WebView source={{ uri: 'https://google.com' }} style={{}} />
+      <ScrollView
+        style={styles.scrollContainer}
+        onLayout={onLayout}
+        refreshControl={
+          <RefreshControl
+            refreshing={false}
+            enabled={false}
+            onRefresh={onRefresh}
+            tintColor={TOMATO}
+            colors={[WHITE]}
+            style={styles.refreshControl}
+          />
+        }>
+        <WebView
+          source={{ uri: 'https://google.com' }}
+          ref={webview}
+          style={wevViewStyle}
+        />
+      </ScrollView>
     </>
   );
 }
